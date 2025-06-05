ea367366d973eb8143d8f02b080d100ae96be55a
olexandrmazur <olxsamr.06g@gmail.com>
2025-06-05 10:37:26 +0300

    Add files via upload

---
+++ Java/models/User.java
@@ -0,0 +1,27 @@
+package models;
+
+public class User {
+
+    private String password;
+    private String login;
+    public User(String login, String password) {
+        this.password = password;
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+}
